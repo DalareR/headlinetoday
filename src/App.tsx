834fcb73f1f7bada01be81b7d6a966bd91213e4b
Dalare Rana <dalarebuddy@gmail.com>
2023-04-10 19:49:42 -0400

    Created NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import NavBar from "./components/NavBar";
+
 const key = `6931e5d37075445a96f6b0850b424f03`;
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+import { HStack, Stack, Text } from "@chakra-ui/react";
+
+function NavBar() {
+  return (
+    <HStack justifyContent="center">
+      <Text textStyle="h1" color="brand.100">
+        Headline.
+      </Text>
+    </HStack>
+  );
+}
+
+export default NavBar;
