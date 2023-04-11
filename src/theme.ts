0f7a26107fd53ee134153a1e1cd05fd2dadd48dd
Dalare Rana <dalarebuddy@gmail.com>
2023-04-10 20:23:41 -0400

    added font and changed the font style in theme.ts

--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,15 @@
 import { extendTheme } from "@chakra-ui/react";
 
+const styles = {
+  global: {
+    body: {
+      bg: "#CCCCCC",
+      color: "#323232",
+      fontFamily: `'Playfair Display', serif`,
+    },
+  },
+};
+
 const colors = {
   brand: {
     100: "#B10000",
@@ -23,6 +33,11 @@ const textStyles = {
     fontWeight: "bold",
     lineHeight: "100%",
   },
+  h4: {
+    fontSize: ["1rem", "1.5rem"],
+    fontWeight: "bold",
+    lineHeight: "100%",
+  },
 };
 
-export const theme = extendTheme({ textStyles, colors });
+export const theme = extendTheme({ textStyles, colors, styles });
