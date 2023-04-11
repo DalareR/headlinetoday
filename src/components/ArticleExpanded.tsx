b39ceba0085d765540eaec46c06877879b8f1c4c
Dalare Rana <dalarebuddy@gmail.com>
2023-04-11 13:25:09 -0400

    articleExpanded compon shows the selected article details

--- src/components/ArticleExpanded.tsx
+++ src/components/ArticleExpanded.tsx
@@ -1,35 +1,38 @@
-import {
-  Box,
-  Flex,
-  HStack,
-  Image,
-  Stack,
-  Text,
-  VStack,
-} from "@chakra-ui/react";
+import { Box, Flex, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import { Article } from "../hooks/useGetArticles";
 
-function ArticleExpanded() {
+interface Props {
+  article: Article;
+}
+
+function ArticleExpanded({ article }: Props) {
   return (
     <Box
       h="100%"
+      overflow="scroll"
       borderRadius="15px"
       boxShadow="5px 5px 15px rgba(0,0,0, .3)"
+      m="10px"
       p="15px"
     >
-      <Flex direction="column">
-        <Box flex="1" mb="15px">
-          <Image w="400px" h="400px" bg="gray" borderRadius="15px" />
-        </Box>
-        <VStack alignItems="start" flex="1">
-          <Text textStyle="h3">This is a title</Text>
-          <Text>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
-            beatae aut commodi, error modi ex voluptas reprehenderit assumenda
-            autem sit.
-          </Text>
-          <HStack justifyContent="space-between">
-            <Text textStyle="h4">Author: Michael Jr.</Text>
-            <Text textStyle="h4">01/02/2023</Text>
+      <Flex h="100%" direction="column">
+        <Image
+          w="100%"
+          flex="1"
+          objectFit="cover"
+          objectPosition="center"
+          borderRadius="15px"
+          src={article.urlToImage}
+          mb="15px"
+        />
+        <VStack flex="1" mt="15px" justifyContent="space-between">
+          <VStack spacing="15px">
+            <Text textStyle="h3">{article.title}</Text>
+            <Text>{article.description}</Text>
+          </VStack>
+          <HStack w="100%" justifyContent="space-between" pt="20px">
+            <Text textStyle="h4">{article.author}</Text>
+            <Text textStyle="h4">{article.publishedAt}</Text>
           </HStack>
         </VStack>
       </Flex>
