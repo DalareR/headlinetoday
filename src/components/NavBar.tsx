597169279ea5f6f708576b4ddeac1e93e705093e
Dalare Rana <dalarebuddy@gmail.com>
2023-04-11 13:23:57 -0400

    changed the title and added padding

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,10 @@
-import { HStack, Stack, Text } from "@chakra-ui/react";
+import { HStack, Text } from "@chakra-ui/react";
 
 function NavBar() {
   return (
-    <HStack justifyContent="center">
-      <Text textStyle="h1" color="brand.100">
-        Headline.
+    <HStack justifyContent="center" textAlign="center">
+      <Text textStyle="h1" color="brand.100" p="5px">
+        Headline Today
       </Text>
     </HStack>
   );
